6d3df1e288c419ba907ef76b6ac0a5a0269ebe3c
wagih soudi <149485629+wagehsoudi@users.noreply.github.com>
2026-05-20 18:07:09 +0300

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,55 @@
+<h1 align="center">Hi 👋, I'm Wagih Soudi</h1>
+<h3 align="center">QA Specialist | Test Automation Engineer</h3>
+
+---
+
+- 🔭 I’m currently working on **Llumen**
+- 🌱 I’m currently learning **ISTQB Foundation Level**
+- 📫 How to reach me: **wagehmohamedsoudi1@gmail.com**
+- 📍 Based in Cairo, Egypt
+
+---
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://www.linkedin.com/in/wagih-soudi-070233361/" target="blank">
+  <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="Wagih Soudi" height="30" width="40" />
+</a>
+</p>
+
+---
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left">
+  <a href="https://www.cypress.io" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/cypress.svg" alt="cypress" width="40" height="40"/>
+  </a>
+  <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+    <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/>
+  </a>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/>
+  </a>
+  <a href="https://www.python.org" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/>
+  </a>
+  <a href="https://playwright.dev/" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/playwright.svg" alt="playwright" width="40" height="40"/>
+  </a>
+  <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/>
+  </a>
+</p>
+
+---
+
+### 📊 Certifications
+- ISTQB Foundation Level - ITI Platform
+- Manual Testing - QAcart
+
+ 📊 Certifications
+- ISTQB Foundation Level from Bisics to certificate ITI Platform
+- Manual Testing training with QAcart. 
+
+---
+
