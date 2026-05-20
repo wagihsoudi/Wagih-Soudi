39a740293594812e7e8b388d2e6507d228230441
wagih soudi <149485629+wagihsoudi@users.noreply.github.com>
2026-05-20 18:39:17 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,50 +1,32 @@
-<h1 align="center">Hi 👋, I'm Wagih Soudi</h1>
-<h3 align="center">QA Specialist | Test Automation Engineer</h3>
 
----
+ Hi there! 👋 I'm Wagih Soudi
 
-- 🔭 I’m currently working on **Llumen**
-- 🌱 I’m currently learning **ISTQB Foundation Level**
-- 📫 How to reach me: **wagehmohamedsoudi1@gmail.com**
+ Software Quality Assurance Engineer | Test Automation Enthusiast
+
+- 🔭 Currently working as a **QA Engineer**
+- 🌱 Learning **Test Automation**, , **Playwright**, and **Cypress**
+- 💡 Passionate about delivering high-quality software through effective testing
+- 📫 How to reach me: `Wagehmohamedsoudi1@gmail.com`
 - 📍 Based in Cairo, Egypt
 
 ---
 
-<h3 align="left">Connect with me:</h3>
-<p align="left">
-<a href="https://www.linkedin.com/in/wagih-soudi-070233361/" target="blank">
-  <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="Wagih Soudi" height="30" width="40" />
-</a>
-</p>
+ 🛠️ Skills & Tools
+
+- Manual Testing • Test Case Design** • Bug Reporting**
+- Automation Testing: Selenium • Playwright • Cypress
+- Programming: Python • JavaScript
+---
+
+ 📌 Featured Projects
+
+- [Llumen] - The Decision Interface
+Llumen creates a direct path from data to understanding. It unifies context, visualization, and AI so leaders can see what matters, explore it effortlessly, and move with confidence when decisions need to be made.
 
 ---
 
-<h3 align="left">Languages and Tools:</h3>
-<p align="left">
-  <a href="https://www.cypress.io" target="_blank" rel="noreferrer">
-    <img src="https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/cypress.svg" alt="cypress" width="40" height="40"/>
-  </a>
-  <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
-    <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/>
-  </a>
-  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
-    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/>
-  </a>
-  <a href="https://www.python.org" target="_blank" rel="noreferrer">
-    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/>
-  </a>
-  <a href="https://playwright.dev/" target="_blank" rel="noreferrer">
-    <img src="https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/playwright.svg" alt="playwright" width="40" height="40"/>
-  </a>
-  <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
-    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/>
-  </a>
-</p>
+ 📊 Certifications
+- ISTQB Foundation Level from Bisics to certificate ITI Platform
+- Manual Testing training with QAcart. 
 
 ---
-
-### 📊 Certifications
-- ISTQB Foundation Level - ITI Platform
-- Manual Testing - QAcart
-
-Remember, GitHub is not just a platform for code, it's a space for connecting like-minded individuals, sharing knowledge, and growing together. Keep exploring, keep coding, and never stop learning! 🌟
