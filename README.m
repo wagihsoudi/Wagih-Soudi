f331d2606c0cd5d60ba563697f2fe9ca5827f9c4
wagih soudi <149485629+wagihsoudi@users.noreply.github.com>
2026-05-20 18:32:43 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,33 +1,59 @@
-<h1 align="center">Hi 👋, I'm Wagih Soudi</h1>
-<h3 align="center">QA Specialist | Test Automation Engineer</h3>
+<img src="https://capsule-render.vercel.app/api?type=venom&height=220&color=0:000000,100:a371f7&text=Wagih%20Soudi&fontSize=60&fontColor=FFFFFF&animation=fadeIn&fontAlignY=40&desc=QA%20Specialist&descSize=22&descColor=FFFFFF&descAlignY=65" width="100%"/>
 
----
+<p align="center">
+  <a href="https://komarev.com/ghpvc/?username=wagihsoudi">
+    <img src="https://komarev.com/ghpvc/?username=wagihsoudi&label=Profile%20views&color=00FFFF&style=flat-square" alt="wagihsoudi's profile views" />
+  </a>
+</p>
 
-### 👨‍💼 About Me
-- 🔭 Currently working on **Llumen**
-- 🌱 Currently learning **ISTQB Foundation Level**
-- 📫 How to reach me: [wagehmohamedsoudi1@gmail.com](mailto:wagehmohamedsoudi1@gmail.com)
-- 📍 Cairo, Egypt
+<img src="https://i.pinimg.com/originals/e2/9c/75/e29c75880a73d8a049c09ceca6eaa666.gif" alt="Banner" width="100%" />
 
----
+## 📌 About Me
+- 🔭 Currently working as a QA Engineer
+- 🌱 Learning Test Automation,  Playwright , and  Cypress
+- 💡 Passionate about delivering high-quality software through effective testing
+-  🛠️ Manual Testing • Test Case Design** • Bug Reporting**
+-  🛠️ Automation Testing: Selenium • Playwright • Cypress
+- 🛠️ Programming: Python • JavaScript
+- Featured Projects:
+- 📌 Llumen : The Decision Interface -Llumen creates a direct path from data to understanding. It unifies context, visualization, and AI so leaders can see what matters, explore it effortlessly, and move with confidence when decisions need to be made.
 
-### Connect with me
-[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/wagih-soudi-070233361/)
 
----
+## 📊 GitHub Stats & Trophies
+<p align="center">
+  <img src="https://trophy.ryglcloud.net/?username=wagihsoudi&theme=radical&no-frame=true&no-bg=true&margin-w=4&cache_seconds=86400" alt="Wagih Soudi's GitHub Trophies" />
+</p>
 
-### 🛠 Languages and Tools
 
-![Cypress](https://img.shields.io/badge/Cypress-17202C?style=for-the-badge&logo=cypress&logoColor=white)
-![Playwright](https://img.shields.io/badge/Playwright-2EAD33?style=for-the-badge&logo=playwright&logoColor=white)
-![Selenium](https://img.shields.io/badge/Selenium-43B02A?style=for-the-badge&logo=selenium&logoColor=white)
-![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
-![TypeScript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)
-![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
-![Figma](https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white)
+## 🛠️ Languages & Tools
 
----
+<h3 align="center">Programming Languages</h3>
+<p align="center">
+  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="JavaScript" width="40" />
+  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="TypeScript" width="40" />
 
-### Certifications
-- **ISTQB Foundation Level** - ITI Platform
-- **Manual Testing** - QAcart
+</p>
+
+<h3 align="center">Tools</h3>
+<p align="center">
+  <img src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg" alt="VS Code" width="40" />
+  <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="Figma" width="40" />
+
+</p>
+
+## 🔗 Connect with Me
+<p align="center">
+  <a href="https://www.linkedin.com/in/wagih-soudi-070233361/?skipRedirect=true">
+    <img src="https://raw.githubusercontent.com/gauravghongde/social-icons/master/SVG/Color/LinkedIN.svg" alt="LinkedIn" width="40" />
+  </a>
+  <a href="mailto:wagehmohamedsoudi1@gmail.com">
+    <img src="https://raw.githubusercontent.com/gauravghongde/social-icons/master/SVG/Color/Gmail.svg" alt="Gmail" width="40" />
+  </a>
+</p>
+
+<p align="center"><a href="https://www.buymeacoffee.com/chamidudili" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a></p>
+
+<div align="center">
+  <img src="https://user-images.githubusercontent.com/74038190/212284115-f47cd8ff-2ffb-4b04-b5bf-4d1c14c0247f.gif" alt="Bottom Line" width="100%" />
+</div>
+
